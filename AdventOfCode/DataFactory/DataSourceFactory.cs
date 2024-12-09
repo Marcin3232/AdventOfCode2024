b689b282800add3f0f10b0be4b4fa582e388ebbd
mopiolka <mopiolka@aiut.com.pl>
2024-12-09 14:47:06 +0100

    Add dataSource factory

---
+++ AdventOfCode/DataFactory/DataSourceFactory.cs
@@ -0,0 +1,50 @@
+﻿using DataFactory.Interfaces;
+using DataFactory.Services;
+
+namespace DataFactory;
+
+public class DataSourceFactory : IDataSourceFactory
+{
+    private readonly string _baseDirectory;
+
+    public DataSourceFactory()
+    {
+        _baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    }
+
+    public string[]? GetData(int dayNumber)
+    {
+        var fileName = GetFileName(dayNumber);
+        if (string.IsNullOrEmpty(fileName))
+            return null;
+
+        IDataSources dataSource = new DataSources();
+
+        return dataSource.GetData(Path.Combine(GetPath(dayNumber), fileName));
+    }
+
+    private string GetPath(int dayNumber) =>
+        Path.Combine(_baseDirectory, $"Resources/{dayNumber}");
+
+    private string GetFileName(int dayNumber)
+    {
+        var path = GetPath(dayNumber);
+        if (Directory.Exists(path))
+        {
+            string? file = Directory.GetFiles(path).FirstOrDefault();
+
+            if (file is null)
+            {
+                Console.WriteLine("Dictionary is empty.");
+                return string.Empty;
+            }
+
+            return file;
+        }
+        else
+        {
+            Console.WriteLine("Dictionary not exist");
+            return string.Empty;
+        }
+    }
+}
--- AdventOfCode/DaysFactory/Factory.cs
+++ AdventOfCode/DaysFactory/Factory.cs
@@ -1,4 +1,6 @@
 ﻿using Common.Interfaces;
+using DataFactory;
+using DataFactory.Interfaces;
 using Day1;
 using Day2;
 using DaysFactory.Enums;
@@ -7,12 +9,19 @@ namespace DaysFactory;
 
 public class Factory : IFactory
 {
+    private readonly IDataSourceFactory _dataSource;
+    public Factory()
+    {
+        _dataSource = new DataSourceFactory();
+    }
 
     public IDayProject? CreateProject(DayNumber dayNumber) => dayNumber switch
     {
-        DayNumber.DayOne => new DayOne(),
-        DayNumber.DayTwo => new DayTwo(),
+        DayNumber.DayOne => new DayOne(GetData(dayNumber)),
+        DayNumber.DayTwo => new DayTwo(GetData(dayNumber)),
         _ => null
     };
 
+    private string[] GetData(DayNumber dayNumber) =>
+       _dataSource.GetData((int)dayNumber) ?? [];
 }
